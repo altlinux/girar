bd501f876afef4e604f720448c0c826b519c7a6d
Dmitry V. Levin <ldv@altlinux.org>
2006-09-08 22:26:10 +0000

    giter-sh.c: Rewrite in more maintainable way.

--- bin/giter-sh.c
+++ bin/giter-sh.c
@@ -10,31 +10,30 @@
 #include <sys/types.h>
 #include <pwd.h>
 
+static void shell(char *av[]);
+
 int
 main (int ac, char *av[])
 {
-	const char prefix[] = "git_";
-	char   *home;
-	struct passwd *pw;
 	uid_t   uid = getuid();
-
 	if (!uid)
 		error(EXIT_FAILURE, 0, "must be non-root");
 
-	/* setcred */
-	if (setuid(uid) < 0)
-		error(EXIT_FAILURE, errno, "setuid");
-
-	pw = getpwuid(uid);
+	struct passwd *pw = getpwuid(uid);
 	if (!pw)
 		error(EXIT_FAILURE, errno, "getpwuid");
 
+	/* check for prefix, be quiet about failure */
+	const char prefix[] = "git_";
 	if (strncmp(pw->pw_name, prefix, sizeof(prefix) - 1))
 		error(EXIT_FAILURE, 0, "prefix not found");
 
-	if (asprintf(&home, "/people/%s", pw->pw_name + sizeof(prefix) - 1) < 0)
+	char   *home;
+	if (asprintf(&home, "%s%s",
+		     "/people/", pw->pw_name + sizeof(prefix) - 1) < 0)
 		error(EXIT_FAILURE, errno, "asprintf");
 
+	/* chdir to new home, be quiet about failure */
 	if (chdir(home) < 0)
 		error(EXIT_FAILURE, errno, "chdir");
 
@@ -55,132 +54,100 @@ main (int ac, char *av[])
 		error(EXIT_FAILURE, errno, "setenv: PATH");
 
 	if (3 == ac)
-	{
-		const char git_receive_pack[] = "git-receive-pack ";
-		const char git_upload_pack[] = "git-upload-pack ";
-		const char init_db[] = "git-init-db ";
-		const char rm_db[] = "git-rm-db ";
-		const char clone[] = "git-clone ";
-
-		if (strcmp("-c", av[1]))
-			error(EXIT_FAILURE, EINVAL, "%s", av[1]);
-
-		if (!strcmp("help", av[2]))
-		{
-			printf(
-"Available commands:\n"
-"%s<directory>\n"
-"%s<directory>\n"
-"%s<directory>\n"
-"%s<directory>\n"
-"%s<repository> [<directory>]\n"
-"list\n"
-"help\n",
-				git_receive_pack,
-				git_upload_pack,
-				init_db,
-				rm_db,
-				clone);
-			return EXIT_SUCCESS;
-		}
-
-		if (!strcmp("list", av[2]))
-		{
-			const char *args[] = {"ls", "-log", "packages", NULL};
-			execv("/bin/ls",
-			      (char *const *) args);
-			error(EXIT_FAILURE, errno, "execv: %s", args[0]);
-		}
-
-		if (!strncmp(git_receive_pack, av[2], sizeof(git_receive_pack) - 1) ||
-		    !strncmp(git_upload_pack, av[2], sizeof(git_upload_pack) - 1))
-		{
-			av[0] = (char *) "git-shell";
-			execv("/usr/bin/git-shell", av);
-			error(EXIT_FAILURE, errno, "execv: %s", av[0]);
-		}
-
-		if (!strncmp(init_db, av[2], sizeof(init_db) - 1))
-		{
-			const char *args[strlen(av[2])];
-			unsigned i = 0;
-			char   *p = av[2] + sizeof(init_db) - 1;
-
-			args[i++] = "people-init-db";
-
-			while (*p)
-			{
-				args[i++] = p++;
-				while (*p && !isblank(*p))
-					p++;
-				if (!*p)
-					break;
-				*(p++) = '\0';
-				while (isblank(*p))
-					p++;
-			}
-
-			args[i] = 0;
-			execv("/usr/local/bin/people-init-db",
-			      (char *const *) args);
-			error(EXIT_FAILURE, errno, "execv: %s", args[0]);
-		}
-
-		if (!strncmp(rm_db, av[2], sizeof(rm_db) - 1))
-		{
-			const char *args[strlen(av[2])];
-			unsigned i = 0;
-			char   *p = av[2] + sizeof(rm_db) - 1;
-
-			args[i++] = "people-rm-db";
-
-			while (*p)
-			{
-				args[i++] = p++;
-				while (*p && !isblank(*p))
-					p++;
-				if (!*p)
-					break;
-				*(p++) = '\0';
-				while (isblank(*p))
-					p++;
-			}
-
-			args[i] = 0;
-			execv("/usr/local/bin/people-rm-db",
-			      (char *const *) args);
-			error(EXIT_FAILURE, errno, "execv: %s", args[0]);
-		}
-
-		if (!strncmp(clone, av[2], sizeof(clone) - 1))
-		{
-			const char *args[strlen(av[2])];
-			unsigned i = 0;
-			char   *p = av[2] + sizeof(clone) - 1;
-
-			args[i++] = "people-clone";
-
-			while (*p)
-			{
-				args[i++] = p++;
-				while (*p && !isblank(*p))
-					p++;
-				if (!*p)
-					break;
-				*(p++) = '\0';
-				while (isblank(*p))
-					p++;
-			}
-
-			args[i] = 0;
-			execv("/usr/local/bin/people-clone",
-			      (char *const *) args);
-			error(EXIT_FAILURE, errno, "execv: %s", args[0]);
-		}
-
-		error(EXIT_FAILURE, EINVAL, "%s", av[2]);
-	}
+		shell(av);
 
 	error(EXIT_FAILURE, 0, "invalid arguments");
 	return EXIT_FAILURE;
+}
+
+typedef struct
+{
+	const char *name, *exec, *usage;
+} cmd_t;
+
+static cmd_t commands[] = {
+	{"git-init-db", "/usr/local/bin/people-init-db", " <directory>"},
+	{"git-rm-db", "/usr/local/bin/people-rm-db", " <directory>"},
+	{"git-clone", "/usr/local/bin/people-clone", " <repository> [<directory>]"}
+};
+
+static void exec_cmd(cmd_t *cmd, char *str);
+
+static void
+shell (char *av[])
+{
+	const char git_receive_pack[] = "git-receive-pack ";
+	const char git_upload_pack[] = "git-upload-pack ";
+
+	if (strcmp("-c", av[1]))
+		error(EXIT_FAILURE, EINVAL, "%s", av[1]);
+
+	unsigned i;
+	if (!strcmp("help", av[2]))
+	{
+		printf("Available commands:\n"
+		       "%s<directory>\n"
+		       "%s<directory>\n",
+		       git_receive_pack,
+		       git_upload_pack);
+		for (i = 0; i < sizeof(commands)/sizeof(commands[0]); ++i)
+			printf("%s%s\n", commands[i].name, commands[i].usage);
+		printf("list\nhelp\n");
+		exit(EXIT_SUCCESS);
+	}
+
+	if (!strcmp("list", av[2]))
+	{
+		const char *args[] = { "ls", "-log", "packages", NULL };
+		execv("/bin/ls", (char *const *) args);
+		error(EXIT_FAILURE, errno, "execv: %s", args[0]);
+	}
+
+	if (!strncmp(git_receive_pack, av[2], sizeof(git_receive_pack) - 1) ||
+	    !strncmp(git_upload_pack, av[2], sizeof(git_upload_pack) - 1))
+	{
+		av[0] = (char *) "git-shell";
+		execv("/usr/bin/git-shell", av);
+		error(EXIT_FAILURE, errno, "execv: %s", av[0]);
+	}
+
+	for (i = 0; i < sizeof(commands)/sizeof(commands[0]); ++i)
+	{
+		size_t len = strlen(commands[i].name);
+		if (!strncmp(commands[i].name, av[2], len) &&
+		    isblank(av[2][len]))
+			exec_cmd(&commands[i], &av[2][len]);
+	}
+
+	error(EXIT_FAILURE, EINVAL, "%s", av[2]);
+}
+
+static void exec_cmd(cmd_t *cmd, char *str)
+{
+	const char *slash = strrchr(cmd->exec, '/');
+	if (slash)
+		++slash;
+
+	const char *args[strlen(str) + 2];
+	unsigned i = 0;
+	args[i++] = slash;
+
+	char   *p = str;
+	while (*p && isblank(*p))
+		++p;
+	while (*p)
+	{
+		args[i++] = p++;
+		while (*p && !isblank(*p))
+			++p;
+		if (!*p)
+			break;
+		*(p++) = '\0';
+		while (isblank(*p))
+			++p;
+	}
+
+	args[i] = 0;
+	execv(cmd->exec, (char *const *) args);
+	error(EXIT_FAILURE, errno, "execv: %s", args[0]);
 }
